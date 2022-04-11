10e7694be0129c088c3e116c9d86454d11c864e2
Alberto Hernandez <Alhern8542@gmail.com>
2022-04-11 18:20:40 -0500

    feat: Allows user to choose grid size

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
+const color = document.querySelector("#colorPicker");
+
 // Select size input
+const height = document.querySelector("#inputHeight");
+const width = document.querySelector("#inputWidth");
 
 // When size is submitted by the user, call makeGrid()
+const form = document.querySelector("#sizePicker");
+const submit = document.querySelector("#inputSub");
+const canvas = document.querySelector("#pixelCanvas");
 
-function makeGrid() {
+function makeGrid(e) {
+    e.preventDefault();
 
-// Your code goes here!
+    for(let x = 0; x < height.value; x++){
+        let row = document.createElement("tr");
+        row.id = `row${x}`;
+        canvas.appendChild(row);
+        for(let y = 0; y < width.value; y++){
+            let column = document.createElement("td");
+            row.appendChild(column);
+        }
+    }
 
+    console.log(height.value);
+    console.log(width.value);
+    console.log("Is this working?");
 }
+
+submit.addEventListener("click", makeGrid);
+
