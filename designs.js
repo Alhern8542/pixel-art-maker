15e5317b0690367dd7b863863e4b10042dc59b17
Alberto Hernandez <Alhern8542@gmail.com>
2022-04-11 21:51:54 -0500

    feat: User can choose color

--- designs.js
+++ designs.js
@@ -6,29 +6,23 @@ const height = document.querySelector("#inputHeight");
 const width = document.querySelector("#inputWidth");
 
 // When size is submitted by the user, call makeGrid()
-const form = document.querySelector("#sizePicker");
 const submit = document.querySelector("#inputSub");
 const canvas = document.querySelector("#pixelCanvas");
 
 function makeGrid(e) {
     e.preventDefault();
-
     for(let x = 0; x < height.value; x++){
         let row = document.createElement("tr");
-        row.id = `row${x}`;
         canvas.appendChild(row);
         for(let y = 0; y < width.value; y++){
-            let column = document.createElement("td");
-            row.appendChild(column);
+            let pixel = document.createElement("td");
+            pixel.addEventListener("click", function(){
+                pixel.style.backgroundColor = color.value;
+            })
+            row.appendChild(pixel);
         }
     }
     submit.removeEventListener("click", makeGrid);
-    // TODO remove after testing
-    console.log(height.value);
-    console.log(width.value);
-    console.log("Is this working?");
 }
 
 submit.addEventListener("click", makeGrid);
-
-
